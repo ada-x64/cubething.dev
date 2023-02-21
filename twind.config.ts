984a4f740593c1ddd9be5376b7e10396ef48897b
ada mandala <ada.mandala@pm.me>
2023-02-20 21:26:54 -0600

    update to twind v1.x.y

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,10 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { Options } from "twind_fresh_plugin/twind.ts";
+
+// twind preset
+import presetAutoPrefix from "twind-preset-autoprefix";
+import presetTailWind from "twind-preset-tailwind";
 
 export default {
+  presets: [presetAutoPrefix, presetTailWind],
   selfURL: import.meta.url,
 } as Options;
