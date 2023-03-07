55ff22559012b835170c2eda4fa44e69c419bbda
ada mandala <ada.mandala@pm.me>
2023-03-07 16:40:12 -0600

    Add meta tags for previews.

--- components/Head.tsx
+++ components/Head.tsx
@@ -12,6 +12,33 @@ export default function HeadComponent() {
       <link rel="stylesheet" href="/style/code.css" />
       <script src="/scripts/onScroll.js" />
       <script src="/scripts/detectTheme.js"></script>
+
+      {/*<!-- Primary Meta Tags -->*/}
+      <meta name="title" content="< cubething />" />
+      <meta
+        name="description"
+        content="Personal home page of Ada Mandala. Posts about tech, art, philosophy. // Rust, Linux, WASM // Graphics, Games // Metaphysics, Aesthetics"
+      />
+
+      {/*<!-- Open Graph / Facebook -->*/}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://cubething.dev/" />
+      <meta property="og:title" content="< cubething />" />
+      <meta
+        property="og:description"
+        content="Personal home page of Ada Mandala. Posts about tech, art, philosophy. // Rust, Linux, WASM // Graphics, Games // Metaphysics, Aesthetics"
+      />
+      <meta property="og:image" content="/meta/preview.svg" />
+
+      {/*<!-- Twitter -->*/}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://cubething.dev/" />
+      <meta property="twitter:title" content="< cubething />" />
+      <meta
+        property="twitter:description"
+        content="Personal home page of Ada Mandala. Posts about tech, art, philosophy. // Rust, Linux, WASM // Graphics, Games // Metaphysics, Aesthetics"
+      />
+      <meta property="twitter:image" content="/meta/preview.svg" />
     </Head>
   );
 }
