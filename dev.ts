020adbdbc104226666f658f48fc1efad71d6c8bc
ada mandala <ada.mandala@pm.me>
2023-04-24 16:06:32 -0500

    test

--- dev.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env -S deno run -A --cached-only --watch=static/,routes/
-
-import dev from "$fresh/dev.ts";
-
-await dev(import.meta.url, "./main.ts");
--- run.sh
+++
@@ -1,5 +0,0 @@
-
-export DENO_PORT=3000
-export DENO_DEPLOYMENT_ID=28c5dd40e2112ec22558e867872b9adc752cae4f
-deno run -A "/var/www/main.ts"
-
