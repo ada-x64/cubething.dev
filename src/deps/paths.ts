bd81e0f7e3e90a8613ea672474889f97f19305bb
Ada Mandala <ada@cubething.dev>
2024-06-10 21:55:40 -0500

    fix deployment (#14)
    * update about page
    
    * ftrack the .env files
    
    * add debug statements
    
    * more tesing
    
    ---------
    
    Co-authored-by: ada mandala <ada.mandala@pm.me>

--- src/deps/paths.ts
+++ src/deps/paths.ts
@@ -1,10 +1,17 @@
 import { resolve } from "$std/path/mod.ts";
 import * as dotenv from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 
+const root = Deno.env.has("DEPLOY") ? "./src/" : "./";
+
 if (Deno.env.has("PROD")) {
-  await dotenv.load({ envPath: "./prod.env", export: true });
+  await dotenv.load({ envPath: root + "prod.env", export: true });
 } else {
-  await dotenv.load({ envPath: "./dev.env", export: true });
+  await dotenv.load({ envPath: root + "dev.env", export: true });
+}
+
+console.log("root = " + Deno.realPathSync(root));
+for (const item in Deno.readDirSync(root)) {
+  console.log(item.name);
 }
 
 export const CDN_URL = Deno.env.get("CDN_URL");
